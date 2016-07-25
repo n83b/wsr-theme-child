b088224c892d1a421d70260fd6d793001e7c41d7
n83b <nathan@websector.com.au>
2016-07-25 13:37:25 +0930

    Made child theme more generic

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 /**
 * Enqueues child theme stylesheet, loading first the parent theme stylesheet.
 */
-function divi_child_theme_styles() {
+function wsr_child_theme_styles() {
     wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'divi_child_theme_styles', 11 );
+add_action( 'wp_enqueue_scripts', 'wsr_child_theme_styles', 11 );
