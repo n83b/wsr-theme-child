0bf1f192b35a055951aab785e4d95cfe8e6a77a0
n83b <nathan@websector.com.au>
2017-08-01 10:28:23 +0930

    added enqueue for child style.css

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 * Enqueues child theme stylesheet, loading first the parent theme stylesheet.
 */
 function wsr_child_theme_styles() {
-    wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-theme-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() . '/style.css', array('parent-theme-style'), '1.1');
 }
 add_action( 'wp_enqueue_scripts', 'wsr_child_theme_styles', 11 );
